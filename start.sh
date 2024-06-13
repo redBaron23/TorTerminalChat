8a3c2dfca5309205e1e2e132f79beed4e9113bd7
Pato <23110463+redBaron23@users.noreply.github.com>
2024-06-13 15:38:54 -0300

    adjust permissions

--- start.sh
+++ start.sh
@@ -1,20 +1,31 @@
 #!/bin/sh
 
-# Start Tor
-tor -f /etc/tor/torrc &
+# Create the tor group if it doesn't exist
+if ! getent group tor >/dev/null 2>&1; then
+  addgroup -S tor
+fi
+
+# Create the tor user if it doesn't exist
+if ! id -u tor >/dev/null 2>&1; then
+  adduser -S -H -h /dev/null -G tor -g tor tor
+fi
 
 # Check if the hidden_folder volume is mounted
 if [ -d "hidden_service" ]; then
   echo "Custom hidden_service found. Copying to $HIDDEN_SERVICE_PATH."
   # Copy the custom hidden_folder to Tor's hidden service directory
   cp -a hidden_service/. "$HIDDEN_SERVICE_PATH"
-  chmod -R 700 "$HIDDEN_SERVICE_PATH"
-  chown -R root:root "$HIDDEN_SERVICE_PATH"
 else
   echo "No custom hidden_service found. Using default hidden_service directory."
 fi
 
+chown -R tor:tor "$HIDDEN_SERVICE_PATH"
+chmod -R 700 "$HIDDEN_SERVICE_PATH"
+
 echo "The Onion URL is $(cat "$HIDDEN_SERVICE_PATH/hostname")"
 
-# Start your application with Gunicorn
-gunicorn --worker-class eventlet -w 1 --bind 0.0.0.0:80 app:app
+# Start Tor as the tor user
+su -s /bin/sh -c "tor -f /etc/tor/torrc" tor &
+
+# Start your application with Gunicorn as the tor user
+su -s /bin/sh -c "gunicorn --worker-class eventlet -w 1 --bind 0.0.0.0:80 app:app" tor
